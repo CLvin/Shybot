9fb084223822cbff70efe173da21dd5423cb74ca
Marina Uy <rinaruthfellenz@hotmail.com>
2017-03-25 16:17:18 -0400

    Starting init logic

---
+++ DrivingCode/DrivingCode.ino
@@ -0,0 +1,103 @@
+//Driving Code
+
+#include <Arduino.h>
+
+left Motor(1, 2);
+right Motor(3,4);
+
+void setup(){
+gameInit();
+
+}
+
+
+void loop(){
+}
+
+void gameInit(){
+  //facing direction 0
+  //takePhoto
+  turnLeft(90);
+  //facing direction 1
+  //takePhoto
+  turnLeft(90);
+  //facing direction 2
+  //takePhoto
+  turnLeft(90);
+  //facing direction 3
+  //takePhoto
+  //determine best heading based on photos
+  //direction
+  if (direction == 0){
+    turnLeft(90);
+    if (heading == 0){
+      turnRight(45);
+    }
+    else if (heading == 1){
+      turnRight(22.5);
+    }
+    else if (heading == 3){
+      turnLeft(22.5);
+    }
+    else if (heading == 4){
+      turnLeft(45);
+    }
+  }
+  else if (direction == 1){
+    turnLeft(90);
+    turnLeft(90);
+    if (heading == 0){
+      turnRight(45);
+    }
+    else if (heading == 1){
+      turnRight(22.5);
+    }
+    else if (heading == 3){
+      turnLeft(22.5);
+    }
+    else if (heading == 4){
+      turnLeft(45);
+    }
+  }
+  else if (direction == 2){
+    turnRight(90);
+    if (heading == 0){
+      turnRight(45);
+    }
+    else if (heading == 1){
+      turnRight(22.5);
+    }
+    else if (heading == 3){
+      turnLeft(22.5);
+    }
+    else if (heading == 4){
+      turnLeft(45);
+    }
+  }
+  else{
+    if (heading == 0){
+      turnRight(45);
+    }
+    else if (heading == 1){
+      turnRight(22.5);
+    }
+    else if (heading == 3){
+      turnLeft(22.5);
+    }
+    else if (heading == 4){
+      turnLeft(45);
+    }
+  }
+}
+
+void driveStraight(int speed){
+
+}
+
+void turnLeft(int heading){
+
+}
+
+void turnRight(int heading){
+
+}
